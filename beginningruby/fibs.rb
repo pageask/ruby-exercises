2d964bf5b363c5b23a6af44292a473406490f11f
pageask <pageask@gmail.com>
2013-03-17 20:05:25 +0800

    beginningruby fibs.rb define each and include Enumerable

--- beginningruby/fibs.rb
+++ beginningruby/fibs.rb
@@ -1,17 +1,33 @@
 class Fibs
 
-    def self.upto(max)
-      a, b = 1, 1
-      while a <= max
-        yield a
-        a, b = b, a + b
-      end
+  include Enumerable
+
+  def initialize(max)
+    @max = max
+  end
+
+  def each
+    a, b = 1, 1
+    while a <= @max
+      yield a
+      a, b = b, a + b
     end
+  end
 
 end
 
 
 if __FILE__ == $0
-  Fibs.upto(1000) { |f| print f, " " }
+  fibs = Fibs.new(1000)
+
+  fibs.each { |f| print f, " " }
   puts
+
+  fibs.select { |f| f % 2 == 0 }.each { |f| print f, " " }
+  puts
+
+  puts fibs.include?(233)
+  puts fibs.include?(234)
+  puts fibs.max
+  puts fibs.min
 end
