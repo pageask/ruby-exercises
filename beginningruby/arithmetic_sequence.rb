154ccc7d9338a86ec8763739a445a5eb0d082734
pageask <pageask@gmail.com>
2013-03-17 23:14:10 +0800

    beginningruby arithmetic_sequence.rb
    include Enumerable and def each

--- beginningruby/arithmetic_sequence.rb
+++ beginningruby/arithmetic_sequence.rb
@@ -1,5 +1,7 @@
 class ArithmeticSequence
 
+  include Enumerable
+
   def initialize(start, step)
     @start = start
     @step = step
@@ -18,11 +20,21 @@ class ArithmeticSequence
     @changed[key] = value
   end
 
+  def each
+    i = 0
+    loop do
+      yield self[i]
+      i += 1
+    end
+  end
+
 end
 
 
 if __FILE__ == $0
   s = ArithmeticSequence.new(1, 2)
+  p s.take(11)
+  p s.take_while { |e| e <= 10  }
   puts s[4]
   puts s[4]
   s[4] = 2
